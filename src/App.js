e6f942129da8cffde333a64d41131f57fbb6c0b0
vosse <jovicevicdev@gmail.com>
2020-10-17 02:53:14 +0200

    username in navbar

--- src/App.js
+++ src/App.js
@@ -6,16 +6,11 @@ import { Landing } from './components/layout/Landing'
 import { Login } from './components/auth/Login'
 import { Register } from './components/auth/Register'
 import { Feed } from './components/layout/Feed'
+import { UserPage } from './components/layout/UserPage';
 
 export const App = () => {
 
-  // const getUserInfo = async () => {
-  //   try {
-  //     const res = await fetch('http://localhost/auth/info')
-  //   } catch (err) {
-  //     console.log(err.message)
-  //   }
-  // }
+
 
   const checkAuth = async () => {
 
@@ -30,29 +25,62 @@ export const App = () => {
       const parseRes = await res.json()
 
       parseRes === true ? setIsAuth(true) : setIsAuth(false)
+      
+
     } catch (err) {
       console.log(err.message)
     }
   }
 
-  useEffect( () => {
-    checkAuth()
-  }, [])
+  const getUserInfo = () => {
+    try {
 
-  const [isAuth, setIsAuth] = useState(false)
+      // async()
+      // const res = await fetch('http://localhost:5000/user/info', {
+      //   method: 'GET',
+      //   headers: {
+      //     'x-auth-token': localStorage.token
+      //   }
+      // })
+
+      // const parseRes = await res.json()
+
+      // setUser(parseRes)
+
+      fetch('http://localhost:5000/user/info', {
+        method: 'GET',
+        headers: {
+              'x-auth-token': localStorage.token
+            }
+      }).then( res => res.json())
+      .then(data => setUser(data))
+
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
 
   const setAuth = (x) => {
     setIsAuth(x)
+    getUserInfo()
   }
+
+  const [user, setUser] = useState({})
+  const [isAuth, setIsAuth] = useState(false)
+
+  useEffect( () => {
+    checkAuth()
+  }, [])
 
   return (
     <div className="App">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         <React.Fragment>
-          <Navbar isAuth={isAuth} setAuth={setAuth} />
+          <Navbar user={user} isAuth={isAuth} setAuth={setAuth} />
           <Switch>
             <Route exact path='/feed' render={ () => <Feed/> }/>
             <Route exact path='/signup' render={ (props) => !isAuth ? ( <Register isAuth={isAuth} setAuth={setAuth} />) : ( <Redirect to='/' /> ) } />
+            <Route exact path='/user/:username' render={ (props) => <UserPage {...props} /> } />
             <Route exact path='/signin' render={ (props) => !isAuth ? ( <Login isAuth={isAuth} setAuth={setAuth} /> ) : ( <Redirect to='/' /> )  } />
             <Route exact path='/' render={ (props) => isAuth ? ( <Home /> ) : ( <Landing /> )} />
           </Switch>
