1a8e8f9fd320e575898d3e8a358a69318f9eb972
vosse <jovicevicdev@gmail.com>
2020-10-19 18:19:13 +0200

    landing styling

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 export const Landing = () => {
     return (
-        <div>
-            Landing
+        <div className='landing-container'>
+
+            <div className='diagonal-box'>
+                <div className='content'>
+                    <div className='landing-title'>
+                        Join the convo
+                        </div>
+                        <div className='subheader'>
+                            {`Text &`}
+                        </div>
+                        <div className='subheader'>
+                            Waste your time <span className='subtle-text'>anonymously</span>
+                        </div>
+                </div>
+            </div>
         </div>
     )
 }
