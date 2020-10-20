5d23384ec0296738202e1173a2d45756c57466c6
vosse <jovicevicdev@gmail.com>
2020-10-20 03:47:10 +0200

    added landing styling classes

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -1,22 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Landing = () => {
     return (
         <div className='landing-container'>
-
-            <div className='diagonal-box'>
-                <div className='content'>
                     <div className='landing-title'>
                         Join the convo
-                        </div>
-                        <div className='subheader'>
-                            {`Text &`}
-                        </div>
-                        <div className='subheader'>
-                            Waste your time <span className='subtle-text'>anonymously</span>
-                        </div>
-                </div>
-            </div>
+                    </div>
+                    <div className='subheader sh'>
+                        {`Text &`}
+                    </div>
+                    <div className='subheader'>
+                        Waste your time <span className='subtle-text'>anonymously</span>
+                    </div>
+                    <div className='signup-btn-container'>
+                        <Link className='landing-signup-btn' to='/signup'>Sign up</Link>
+                    </div>
         </div>
     )
 }
